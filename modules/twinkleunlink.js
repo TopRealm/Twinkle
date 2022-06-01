058da062a4c456b6ecb7b10125d0ba689a14eead
WaitSpring <me@waitspring.com>
2022-06-01 22:21:50 +0800

    ESLint

--- modules/twinkleunlink.js
+++ modules/twinkleunlink.js
@@ -2,7 +2,7 @@
 /**
  * Twinkle.js
  * © 2011-2022 English Wikipedia Contributors
- * This work is licensed under a Creative Commons 
+ * This work is licensed under a Creative Commons
  * Attribution-ShareAlike 3.0 Unported License.
  * https://creativecommons.org/licenses/by-sa/3.0/
  */
--- twinkle.js
+++ twinkle.js
@@ -13,7 +13,7 @@
  * Twinkle.js
  * © 2011-2022 English Wikipedia Contributors
  * © 2021-     Qiuwen Baike Contributors
- * This work is licensed under a Creative Commons 
+ * This work is licensed under a Creative Commons
  * Attribution-ShareAlike 4.0 International License.
  * https://creativecommons.org/licenses/by-sa/4.0/
  */
