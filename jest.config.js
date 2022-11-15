c27cf873032a7b56ceb3eabd5214ed346b114eb2
WaitSpring <me@waitspring.com>
2022-11-15 19:01:55 +0800

    remove jest
    Signed-off-by: WaitSpring <me@waitspring.com>

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-env node */
-
-module.exports = {
-	testMatch: ['**/tests/morebits*.js'],
-	testEnvironment: 'jsdom',
-	setupFilesAfterEnv: ['mock-mediawiki', '<rootDir>/tests/jest.setup.js']
-};
