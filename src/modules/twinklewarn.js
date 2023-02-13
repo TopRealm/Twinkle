05b9f287c13e717e64efe216b18d730449c57635
WaitSpring <me@waitspring.com>
2023-02-13 17:46:51 +0800

    quote-props
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/modules/twinklewarn.js
+++ src/modules/twinklewarn.js
@@ -1,3 +1,4 @@
+/* eslint-disable quote-props */
 "use strict";
 
 /**
@@ -303,7 +304,7 @@ Twinkle.warn.callback = () => {
 //   suppressArticleInSummary (optional): Set to true to suppress showing the article name in the edit summary. Useful if the warning relates to attack pages, or some such.
 Twinkle.warn.messages = {
 	levels: {
-		不同类型的非建设编辑: {
+		"不同类型的非建设编辑": {
 			"uw-vandalism": {
 				level1: {
 					label: "明显的破坏",
@@ -531,7 +532,7 @@ Twinkle.warn.messages = {
 				}
 			}
 		},
-		增加商品或政治广告: {
+		"增加商品或政治广告": {
 			"uw-spam": {
 				level1: {
 					label: "增加不合适的外部链接",
@@ -665,7 +666,7 @@ Twinkle.warn.messages = {
 				}
 			}
 		},
-		翻译品质: {
+		"翻译品质": {
 			"uw-roughtranslation": {
 				level1: {
 					label: "您翻译的质量有待改善",
@@ -681,7 +682,7 @@ Twinkle.warn.messages = {
 				}
 			}
 		},
-		非能接受且违反方针或指引的单方面行为或操作: {
+		"非能接受且违反方针或指引的单方面行为或操作": {
 			"uw-notcensored": {
 				level1: {
 					label: "因为“内容使人反感”而删除条目内容",
@@ -819,7 +820,7 @@ Twinkle.warn.messages = {
 				}
 			}
 		},
-		对其他用户和条目的态度: {
+		"对其他用户和条目的态度": {
 			"uw-npa": {
 				level1: {
 					label: "针对用户的人身攻击",
